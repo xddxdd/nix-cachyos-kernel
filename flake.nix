400388a25bff2ff58039281f665968a125ce0dae
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-02-21 23:38:19 +0100

    feat(flake): substituters

--- flake.nix
+++ flake.nix
@@ -17,6 +17,18 @@
       flake = false;
     };
   };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://attic.xuyh0120.win/lantian"
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
   outputs =
     { self, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } (
