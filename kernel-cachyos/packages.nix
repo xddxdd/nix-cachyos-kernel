469645238b66b0174c32aac4d4a816d13e41b21d
Lan Tian <xuyh0120@outlook.com>
2026-08-16 16:53:12 -0700

    Fix NVIDIA open module build error

--- kernel-cachyos/packages.nix
+++ kernel-cachyos/packages.nix
@@ -29,6 +29,21 @@ lib.mapAttrs' (
           virtualbox = prev.virtualbox.overrideAttrs (old: {
             makeFlags = (old.makeFlags or [ ]) ++ final.kernel.commonMakeFlags;
           });
+
+          # Fix NVIDIA open module build failure
+          # https://hydra.lantian.pub/build/30817/nixlog/4
+          nvidiaPackages = prev.nvidiaPackages // {
+            latest = prev.nvidiaPackages.latest // {
+              open = prev.nvidiaPackages.latest.open.overrideAttrs (old: {
+                postPatch = (old.postPatch or "") + ''
+                  substituteInPlace kernel-open/common/inc/nv-linux.h \
+                    --replace-fail \
+                      "static inline int __to_hwgpio(const struct gpio_device *gdev," \
+                      "static inline int __to_hwgpio(struct gpio_device *gdev,"
+                '';
+              });
+            };
+          };
         }
       )
     );
